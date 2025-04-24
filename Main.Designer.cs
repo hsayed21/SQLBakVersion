c510666737497d86ae97f37faf7908e1f9e920c0
hsayed21 <hsayedhamed21@gmail.com>
2025-04-24 20:20:42 +0200

    🔨 update: add drap-drop file

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,88 +29,136 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtBakFilePath = new System.Windows.Forms.TextBox();
-            this.btnBrowse = new System.Windows.Forms.Button();
             this.lblVersion = new System.Windows.Forms.Label();
+            this.pnlDragDrop = new System.Windows.Forms.Panel();
+            this.lblDragDrop = new System.Windows.Forms.Label();
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlDragDrop.SuspendLayout();
+            this.pnlTitleBar.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(3, 24);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bak File Path:";
-            // 
-            // txtBakFilePath
-            // 
-            this.txtBakFilePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBakFilePath.Location = new System.Drawing.Point(151, 22);
-            this.txtBakFilePath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtBakFilePath.Name = "txtBakFilePath";
-            this.txtBakFilePath.ReadOnly = true;
-            this.txtBakFilePath.Size = new System.Drawing.Size(316, 26);
-            this.txtBakFilePath.TabIndex = 1;
-            // 
-            // btnBrowse
-            // 
-            this.btnBrowse.Location = new System.Drawing.Point(475, 17);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(101, 33);
-            this.btnBrowse.TabIndex = 2;
-            this.btnBrowse.Text = "Browse";
-            this.btnBrowse.UseVisualStyleBackColor = true;
-            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // lblVersion
             // 
             this.lblVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblVersion.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVersion.ForeColor = System.Drawing.Color.LightGreen;
-            this.lblVersion.Location = new System.Drawing.Point(7, 65);
+            this.lblVersion.Location = new System.Drawing.Point(12, 156);
             this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(569, 74);
+            this.lblVersion.Size = new System.Drawing.Size(568, 74);
             this.lblVersion.TabIndex = 3;
             this.lblVersion.Text = "SQL Server 2000";
             this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblVersion.Visible = false;
             // 
+            // pnlDragDrop
+            // 
+            this.pnlDragDrop.AllowDrop = true;
+            this.pnlDragDrop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.pnlDragDrop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDragDrop.Controls.Add(this.lblDragDrop);
+            this.pnlDragDrop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlDragDrop.Location = new System.Drawing.Point(10, 33);
+            this.pnlDragDrop.Name = "pnlDragDrop";
+            this.pnlDragDrop.Size = new System.Drawing.Size(568, 90);
+            this.pnlDragDrop.TabIndex = 1;
+            this.pnlDragDrop.Click += new System.EventHandler(this.pnlDragDrop_Click);
+            this.pnlDragDrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.pnlDragDrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            // 
+            // lblDragDrop
+            // 
+            this.lblDragDrop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDragDrop.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDragDrop.ForeColor = System.Drawing.Color.LightGray;
+            this.lblDragDrop.Location = new System.Drawing.Point(0, 0);
+            this.lblDragDrop.Name = "lblDragDrop";
+            this.lblDragDrop.Size = new System.Drawing.Size(566, 88);
+            this.lblDragDrop.TabIndex = 0;
+            this.lblDragDrop.Text = "Drop .bak file here or click to browse";
+            this.lblDragDrop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDragDrop.Click += new System.EventHandler(this.pnlDragDrop_Click);
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.pnlTitleBar.Controls.Add(this.lblTitle);
+            this.pnlTitleBar.Controls.Add(this.btnClose);
+            this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            this.pnlTitleBar.Size = new System.Drawing.Size(588, 30);
+            this.pnlTitleBar.TabIndex = 4;
+            this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
+            this.pnlTitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseMove);
+            this.pnlTitleBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseUp);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(10, 6);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(108, 17);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "SQL Bak Version";
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseMove);
+            this.lblTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseUp);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(548, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(40, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "✕";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
             // Main
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(588, 153);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(588, 239);
+            this.Controls.Add(this.pnlTitleBar);
             this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.txtBakFilePath);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlDragDrop);
             this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SQL Bak Version";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            this.pnlDragDrop.ResumeLayout(false);
+            this.pnlTitleBar.ResumeLayout(false);
+            this.pnlTitleBar.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtBakFilePath;
-        private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Panel pnlDragDrop;
+        private System.Windows.Forms.Label lblDragDrop;
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
-
--- Main.cs
+++ Main.cs
@@ -9,21 +9,86 @@ namespace SQLBakVersion
     public partial class Main : Form
     {
         private readonly SQLVersion sqlVersion = new SQLVersion();
+        private bool isDragging = false;
+        private Point lastLocation;
+
         public Main()
         {
             InitializeComponent();
+
+            pnlDragDrop.Location = new Point(10, 37);
         }
 
-        private void btnBrowse_Click(object sender, EventArgs e)
+        private void pnlTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                lastLocation = e.Location;
+            }
+        }
+
+        private void pnlTitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X,(this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void pnlTitleBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pnlDragDrop_Click(object sender, EventArgs e)
+        {
+            SelectBakFile();
+        }
+
+        private void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0 && Path.GetExtension(files[0]).ToLower() == ".bak")
+                {
+                    e.Effect = DragDropEffects.Copy;
+                    return;
+                }
+            }
+            e.Effect = DragDropEffects.None;
+        }
+
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0 && Path.GetExtension(files[0]).ToLower() == ".bak")
+            {
+                ProcessBakFile(files[0]);
+            }
+        }
+
+        private void SelectBakFile()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "SQL Server backup files (*.bak)|*.bak";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                txtBakFilePath.Text = openFileDialog.FileName;
+                ProcessBakFile(openFileDialog.FileName);
             }
+        }
 
-            string filePath = txtBakFilePath.Text;
+        private void ProcessBakFile(string filePath)
+        {
+            lblDragDrop.Text = Path.GetFileName(filePath);
+
             string output = string.Empty;
             if (string.IsNullOrEmpty(filePath) || !File.Exists(filePath))
                 output = "Please select a valid .bak file.";
